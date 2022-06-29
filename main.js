b20c41ef2d940969446f6c1922b74c24ad5aef9e
Shane Martin <smartin38@fordham.edu>
2022-06-29 01:08:36 -0400

    listed Japanese car companies

---
+++ main.js
@@ -0,0 +1,11 @@
+const companies = [
+    {name: 'Honda' }
+    {name: 'Toyota'}
+    {name: 'Nissan'}
+    {name: 'Mitsubishi'}
+    {name: 'Mazda'}
+    {name: 'Subaru'}
+    {name: 'Acura'}
+    {name: 'Lexus'}
+    {name: 'Infiniti'}
+]
